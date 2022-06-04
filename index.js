555531158ee323c9d245c5c2b23dc85477f30d40
Mendy <mendyw94@gmail.com>
2022-06-04 19:52:33 -0400

    commit tree 0cd69ea3f4f745f596f6e97bba2400b6792cc0bc

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 /* Your Code Here */
 
+const createEmployeeRecord = function (arr = [firstName, familyName, title, payPerHour]) {
+    return {
+        firstName : arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = function (arr) {
+    return arr.map(record => createEmployeeRecord(record))
+}
+
+function createTimeInEvent (timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    })
+    return this
+}
+
+function createTimeOutEvent (timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    })
+    return this
+}
+
+function hoursWorkedOnDate (employeeRec, date) {
+    return employeeRec.timeOutEvents[0].hour - employeeRec.timeInEvents[0].hour
+}
+
+function wagesEarnedOnDate () {
+    return null
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
